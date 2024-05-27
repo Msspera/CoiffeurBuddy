91f36700aecf3bed914c31e8e1c609350ad44d6b
MatteoSpera <matteo.spera0301@gmail.com>
2024-05-27 14:18:42 -0300

    Models Serviço e Contexto criados

---
+++ Models/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoiffeurBuddy.Models
+{
+	public class Contexto : DbContext
+	{
+		public Contexto(DbContextOptions<Contexto> options): 
+			base(options) { }
+
+		public DbSet<Servico> Servicos { get; set; }
+
+	}
+}
---
+++ Models/Servico.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoiffeurBuddy.Models
+{
+	[Table("Servicos")]
+	public class Servico
+	{
+		[Key] //anotação chave primária
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(90)]
+		public string Descricao	{ get; set; }
+		[Required]
+		public float Valor {  get; set; }
+	}
+}
