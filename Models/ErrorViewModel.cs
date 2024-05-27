8aef07e4a956b9cc0733212757fe0c04985b0de9
MatteoSpera <matteo.spera0301@gmail.com>
2024-05-27 13:05:31 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoiffeurBuddy.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
