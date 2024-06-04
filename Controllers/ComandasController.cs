b989287cb6a6d666fe6247f6c5d2ebd05319c29d
MatteoSpera <matteo.spera0301@gmail.com>
2024-06-04 20:27:31 -0300

    Adicionados Controllers e Views de Comanda e Produto

---
+++ Controllers/ComandasController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CoiffeurBuddy.Models;
+
+namespace CoiffeurBuddy.Controllers
+{
+    public class ComandasController : Controller
+    {
+        private readonly Contexto _context;
+
+        public ComandasController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: Comandas
+        public async Task<IActionResult> Index()
+        {
+            var contexto = _context.Comandas.Include(c => c.Atendimento);
+            return View(await contexto.ToListAsync());
+        }
+
+        // GET: Comandas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comanda = await _context.Comandas
+                .Include(c => c.Atendimento)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (comanda == null)
+            {
+                return NotFound();
+            }
+
+            return View(comanda);
+        }
+
+        // GET: Comandas/Create
+        public IActionResult Create()
+        {
+            ViewData["AtendimentoId"] = new SelectList(_context.Atendimentos, "Id", "Id");
+            return View();
+        }
+
+        // POST: Comandas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,AtendimentoId,ValorTotal,MetodoPagamento")] Comanda comanda)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(comanda);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AtendimentoId"] = new SelectList(_context.Atendimentos, "Id", "Id", comanda.AtendimentoId);
+            return View(comanda);
+        }
+
+        // GET: Comandas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comanda = await _context.Comandas.FindAsync(id);
+            if (comanda == null)
+            {
+                return NotFound();
+            }
+            ViewData["AtendimentoId"] = new SelectList(_context.Atendimentos, "Id", "Id", comanda.AtendimentoId);
+            return View(comanda);
+        }
+
+        // POST: Comandas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,AtendimentoId,ValorTotal,MetodoPagamento")] Comanda comanda)
+        {
+            if (id != comanda.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(comanda);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ComandaExists(comanda.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AtendimentoId"] = new SelectList(_context.Atendimentos, "Id", "Id", comanda.AtendimentoId);
+            return View(comanda);
+        }
+
+        // GET: Comandas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comanda = await _context.Comandas
+                .Include(c => c.Atendimento)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (comanda == null)
+            {
+                return NotFound();
+            }
+
+            return View(comanda);
+        }
+
+        // POST: Comandas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var comanda = await _context.Comandas.FindAsync(id);
+            if (comanda != null)
+            {
+                _context.Comandas.Remove(comanda);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ComandaExists(int id)
+        {
+            return _context.Comandas.Any(e => e.Id == id);
+        }
+    }
+}
