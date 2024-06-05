6d841acb11ab9491133fe334c78f3fab2b225a47
MatteoSpera <matteo.spera0301@gmail.com>
2024-06-05 01:01:41 -0300

    Controller e view de Produto recriados para incluir novos atributos

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -53,7 +53,7 @@ namespace CoiffeurBuddy.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Descricao")] Produto produto)
+        public async Task<IActionResult> Create([Bind("Id,Descricao,Valor,Estoque")] Produto produto)
         {
             if (ModelState.IsValid)
             {
@@ -85,7 +85,7 @@ namespace CoiffeurBuddy.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Descricao")] Produto produto)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Descricao,Valor,Estoque")] Produto produto)
         {
             if (id != produto.Id)
             {
